f548e9e8be792a502279e97a4423e2e7afdec77b
neko113 <alstn113@gmail.com>
2021-07-29 12:48:37 +0900

    729

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import router from "../router/index";
 
-const baseURL = process.env.NODE_ENV == "production" ? "https://test63194.herokuapp.com/" : "http://localhost:3000";
+const baseURL = process.env.NODE_ENV == "production" ? "https://velog-clone-api.herokuapp.com/" : "http://localhost:3000";
 
 axios.defaults.baseURL = baseURL;
 axios.defaults.headers.common["Content-Type"] = "application/json;charset=utf-8";
